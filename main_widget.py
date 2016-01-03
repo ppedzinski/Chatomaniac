b35ec13edd121bf0b5cfa0f613c7ee110f22a271
Pawel <pawelpedzinski1a@gmail.com>
2016-01-03 22:02:23 +0100

    web part added

--- main_widget.py
+++ main_widget.py
@@ -1,9 +1,13 @@
 import time
+import socket
+import _thread
 from PyQt5.QtGui import QTextCursor
 from PyQt5.QtGui import QColor
 from random import randint
 from PyQt5.uic.properties import QtGui
 from PyQt5.uic.properties import QtCore
+from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QObject, pyqtSignal
 
 __author__ = 'Pawel'
 
@@ -16,6 +20,18 @@ class MainWidget(QWidget):
     def __init__(self, user):
         super().__init__()
 
+        self.serverRole = False
+        self.tcp_ip = '127.0.0.1'
+        self.tcp_port = 5005
+        self.buffer_size = 1024
+        self.message = ""
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.serverThread = ServerWaitThread(self.s, self.buffer_size)
+        self.clientThread = ClientWaitThread(self.s, self.buffer_size)
+        self.clientThread.receivedMessage.connect(self.handleReceivedMessage)
+        self.serverThread.receivedMessage.connect(self.handleReceivedMessage)
+
         self.user = user
         self.messageLineEdit = QLineEdit(self)
         self.conversationTextEdit = QTextEdit(self)
@@ -26,6 +42,11 @@ class MainWidget(QWidget):
         self.setConnections()
 
         self.initLayout()
+
+        self.initialize_socket()
+
+    def metoda(self, text):
+        print(text)
 
     def setConversationTextEdit(self):
         self.conversationTextEdit.setReadOnly(True)
@@ -54,3 +75,91 @@ class MainWidget(QWidget):
         self.conversationTextEdit.append(text)
 
         self.messageLineEdit.setText("")
+
+        if self.serverRole:
+            self.serverThread.sendMessageToClient(bytes(text, 'UTF-8'))
+        else:
+            self.clientThread.sendMessageToServer(bytes(text, 'UTF-8'))
+
+    def handleReceivedMessage(self, data):
+        self.conversationTextEdit.append(data)
+
+    def initialize_socket(self):
+        try:
+            self.create_server()
+            self.serverThread.begin()
+            self.serverRole = True
+        except:
+            self.s.connect((self.tcp_ip, self.tcp_port))
+            #self.s.send(bytes(self.message, 'UTF-8'))
+            self.clientThread.begin()
+
+    def create_server(self):
+        print("create server")
+        i = self.s.bind((self.tcp_ip, self.tcp_port))
+        print(i)
+        self.s.listen(2)
+
+
+class ServerWaitThread(QThread):
+    receivedMessage = pyqtSignal(str)
+    def __init__(self, s, buffer_size):
+        QThread.__init__(self)
+        self.s = s
+        self.buffer_size = buffer_size
+        self.conn = None
+
+    def __del__(self):
+        self.wait()
+
+    def begin(self):
+        self.start()
+
+    def run(self):
+        self.conn, addr = self.s.accept()
+        print ('Connection address:', addr)
+        while 1:
+            data = self.conn.recv(self.buffer_size)
+            if data:
+                try:
+                    self.receivedMessage.emit(data.decode("utf-8"))
+                except:
+                    print("something goes wrong with receiving data")
+        self.conn.close()
+
+    def sendMessageToClient(self, message):
+        print("sending message to client")
+        try:
+            self.conn.send(message)
+        except:
+            print("Client is not yet there")
+
+class ClientWaitThread(QThread):
+    receivedMessage = pyqtSignal(str)
+    def __init__(self, s, buffer_size):
+        QThread.__init__(self)
+        self.s = s
+        self.buffer_size = buffer_size
+
+    def __del__(self):
+        self.wait()
+
+    def begin(self):
+        self.start()
+
+    def run(self):
+        while 1:
+            data = self.s.recv(self.buffer_size)
+            if data:
+                try:
+                    self.receivedMessage.emit(data.decode("utf-8"))
+                except:
+                    print("something goes wrong with receiving message")
+        self.s.close()
+
+    def sendMessageToServer(self, message):
+        print("sending message to server")
+        try:
+            self.s.send(message)
+        except:
+            print("something goes wrong with sending message to server")
